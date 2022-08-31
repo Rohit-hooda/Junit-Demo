1fc555a33c4975888ed65e3aaf2582f8488015c9
Rohit Hooda <rohithooda1999@gmail.com>
2022-08-31 13:27:12 -0400

    This is the Start of the Junit Excercise

---
+++ src/test/java/mathTest.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class mathTest {
+
+    @Test
+    void add() {
+        System.out.println("Add class test case");
+//        fail("This failed");
+
+    }
+}
